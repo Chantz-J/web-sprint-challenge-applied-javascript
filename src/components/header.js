ebdaa936179a4772ccf6885dcf7ec019db10b92a
Chantz-J <johnson.chantz@gmail.com>
2021-02-12 14:26:53 -0500

    Finished task 1 and 2. But Task 2 is still techincally bugged.

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,33 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  // const headerContainer = document.querySelector('.header-container')
+
+  // Create instances
+  const header = document.createElement('div')
+  const headerSpan = document.createElement('span')
+  const headerH1 = document.createElement('h1')
+  const headerSpan2 = document.createElement('span')
+
+  // Create the tree
+  header.appendChild(headerSpan)
+  header.appendChild(headerH1)
+  header.appendChild(headerSpan2)
+
+  //Set classes for elements
+  header.className = 'header'
+  headerSpan.className = 'date'
+  headerSpan2.className = 'temp'
+
+  // Set arguments in the place of Node text
+  headerH1.textContent = title
+  headerSpan.textContent = date
+  headerSpan2.textContent = temp
+
+  // return header
+  return header
 }
+
 
 const headerAppender = (selector) => {
   // TASK 2
@@ -20,6 +46,12 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  const headerContainer = document.querySelector(selector)
+  Header('Some title', 'Some date', 'Some temp')
+  headerContainer.appendChild(Header())
+  return headerContainer
 }
+
+
 
 export { Header, headerAppender }
